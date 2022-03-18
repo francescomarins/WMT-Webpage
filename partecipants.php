a902cd56191207c85842d0cf047329e76059157e
Francesco Marinelli <francesco.marinelli@outlook.com>
2022-03-18 23:47:45 +0100

    participants almost done

--- partecipants.php
+++ partecipants.php
@@ -61,10 +61,10 @@
     array_push($artists, $artist);
   }
 
-  $videostring = "<div>To listen to the song taking part to the contest you can watch the video on the right.</div>";
-  $infostring0 = '<div>Or know more about <a href="';
+  $videostring = "<p>To listen to the song taking part to the contest you can watch the video.</p>";
+  $infostring0 = '<p>Or know more about <a href="';
   $infostring1 = '">';
-  $infostring2 = '</a></div>';
+  $infostring2 = '</a></p>';
   ?>
 </head>
 
@@ -81,15 +81,15 @@
     <h2>2022</h2>
   </header>
   <section id="artists-container">
+    <h3>See the list of all the artists who take part in the copetition. Click on their name to see something more about them.</h3>
     <?php
     foreach ($artists as $singer) {
       echo '<article class="artist-article" id="' . $singer->get_country() . '">';
       echo '<h2 class="artist" onclick=showmore("' . $singer->get_country() . '");>' . $singer->get_name() . ' - ' . $singer->get_song() . '<span class="country">' . $singer->get_country() . '</span></h2>';
       echo '<div class="details" id="' . $singer->get_country() . '-details" style="display: none;">';
-      echo '<span class="musicvideo"> ' . $singer->get_songlink() . '</span>';
-      echo '<div class="about_the_artist">' . '<h2>Information about the artist</h2>' . $videostring . $infostring0 . $singer->get_link() . $infostring1 . $singer->get_name() . $infostring2 .'</div>';
-      echo "</div>";
-      echo '</article>';
+      echo $singer->get_songlink();
+      echo '<span class="about_the_artist">' . '<h2>Information about the artist</h2>' . $videostring . $infostring0 . $singer->get_link() . $infostring1 . $singer->get_name() . $infostring2 .'</span>';
+      echo '</div></article>';
     } ?>
   </section>
 </body>
