52617df35ed9a5bd01beb0bf02dcd3b2ca9a8fd5
Francesco Marinelli <francesco.marinelli@outlook.com>
2022-03-21 15:42:31 +0100

    votes.php started

--- partecipants.php
+++ partecipants.php
@@ -12,41 +12,7 @@
   <link rel="stylesheet" href="mycss.css" type="text/css">
   <script type="text/javascript" src="javascript.js"></script>  <?php
 
-  class Artist {
-    public $name;
-    public $link;
-    public $song;
-    public $songlink;
-    public $country;
-
-    public function __construct($name, $link, $song, $songlink, $country) {
-      $this->name = $name;
-      $this->link = $link;
-      $this->song = $song;
-      $this->songlink = $songlink;
-      $this->country = $country;
-    }
-
-    public function get_name() {
-      return $this->name;
-    }
-
-    public function get_link() {
-      return $this->link;
-    }
-
-    public function get_song() {
-      return $this->song;
-    }
-
-    public function get_songlink() {
-      return $this->songlink;
-    }
-
-    public function get_country() {
-      return $this->country;
-    }
-  }
+  include("artist.php");
 
   $hostname = "localhost";
   $username="root";
--- votes.php
+++ votes.php
@@ -10,6 +10,30 @@
   <title>VOTESxEUROVISION</title>
   <link rel="icon" href="img/logo2.png" type="image/png" sizes="any">
   <link rel="stylesheet" href="mycss.css" type="text/css">
+  <?php
+
+  include("artist.php");
+
+  $hostname = "localhost";
+  $username="root";
+  $password="root";
+  $db="VOTESxEUROVISION";
+  $con = mysqli_connect($hostname, $username, $password, $db) or die('Error connecting to database');
+
+  $artists = array();
+  $result = mysqli_query($con, "SELECT * FROM participants");
+  while($row = mysqli_fetch_assoc($result)){
+    $artist = new Artist($row['name'], $row['link'], $row['song'], $row['songlink'], $row['country']);
+    array_push($artists, $artist);
+  }
+
+  $userquery = "SELECT * FROM users WHERE email=" . $_SESSION['logged'];
+  $userresult = mysqli_query($con, userquery);
+  $userrow = mysqli_fetch_assoc($userresult);
+  $uservotes = $userrow['available_votes'];
+
+  ?>
+
 </head>
   <body>
     <?php session_start(); include("passwords.php"); check_logged(); ?>
@@ -27,7 +51,8 @@
       <h2>2022</h2>
     </header>
     <section id="voting-container">
-      <p>Hello</p>
+      <?php echo "<p> Hi " . $userrow['email'] . "</p>"; ?>
+      <p> Welcome to the voting page! You still have <?php echo "$uservotes"; ?> available! </p>
     </section>
   </body>
 </html>
