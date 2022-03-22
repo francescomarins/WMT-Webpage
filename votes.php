9dc1b189897e52be59802eea0fb71a20e848b98d
Francesco Marinelli <francesco.marinelli@outlook.com>
2022-03-22 15:40:04 +0100

    voting form started

--- votes.php
+++ votes.php
@@ -30,33 +30,48 @@
   ?>
 
 </head>
-  <body>
-    <?php
-    session_start();
-    include("passwords.php");
-    check_logged();
+<body>
+  <?php
+  session_start();
+  include("passwords.php");
+  check_logged();
 
-    $userquery = "SELECT * FROM users WHERE email='" . $_SESSION['logged'] . "'";
-    $userresult = mysqli_query($con, $userquery);
-    $userrow = mysqli_fetch_assoc($userresult);
-    $uservotes = $userrow['available_votes'];
-    ?>
+  $userquery = "SELECT * FROM users WHERE email='" . $_SESSION['logged'] . "'";
+  $userresult = mysqli_query($con, $userquery);
+  $userrow = mysqli_fetch_assoc($userresult);
+  $uservotes = $userrow['available_votes'];
+  ?>
 
-    <nav id="menu">
-      <a class="logo" href="index.php">VOTESxEUROVISION</a>
-      <a href="index.php">Home</a>
-      <a href="partecipants.php">Participants</a>
-      <a href="">Statistics</a>
-      <form action="logout.php" method="post">
-        <button type="submit" id="logout" name="logout" value="logout">Logout</button>
-      </form>
-    </nav>
-    <header id="header">
-      <h1>Eurovision Voting Page</h1>
-      <h2>2022</h2>
-    </header>
-    <section id="voting-container" class="centred">
-      <h2> Welcome to the voting page! You still have <?php echo "$uservotes"; ?> votes available! </h2>
-    </section>
-  </body>
+  <nav id="menu">
+    <a class="logo" href="index.php">VOTESxEUROVISION</a>
+    <a href="index.php">Home</a>
+    <a href="partecipants.php">Participants</a>
+    <a href="">Statistics</a>
+    <form action="logout.php" method="post">
+      <button type="submit" id="logout" name="logout" value="logout">Logout</button>
+    </form>
+  </nav>
+  <header id="header">
+    <h1>Eurovision Voting Page</h1>
+    <h2>2022</h2>
+  </header>
+  <section id="voting-container">
+    <h2> Welcome to the voting page! You still have <?php echo "$uservotes"; ?> votes available! </h2>
+    <p> In this page you can express the preference for a singer. It is possible to express one vote at a time.
+      If you want to give all your twenty votes to a singer you must repeat the procedure all the times.
+      After voting you can see the live ranking the statistics' page.
+    </p>
+    <form class="voting-form centred" action="votes.php" method="post">
+      <select name="country" id="country">
+        <option value="" disabled selected>Select a country</option>
+        <?php
+        foreach ($artists as $singer) {
+          echo '<option value="' . $singer->get_country() . '">(' . $singer->get_country() . ') ' . $singer->get_name() . ' - ' . $singer->get_song() . '</option>';
+        }
+        ?>
+      </select> <br>
+      <button type="submit" name="operation" value="voting">Vote!</button>
+    </form>
+  </section>
+</body>
 </html>
