da6a2803f2fa1582993772636aa1fd6113a360a2
Francesco Marinelli <francesco.marinelli@outlook.com>
2022-03-27 18:17:22 +0200

    artists details wrong

--- javascript.js
+++ javascript.js
@@ -14,9 +14,9 @@ function exists(input) {
 function check() {
   var ok = false;
   if(exists(document.getElementById("email").value) && exists(document.getElementById("password").value))
-    ok = true;
+  ok = true;
   else
-    alert("Warning, some information are missing! Fill all fields properly.");
+  alert("Warning, some information are missing! Fill all fields properly.");
   return ok;
 }
 
@@ -27,4 +27,115 @@ function showmore(nation) {
   } else {
     el.style.display = "none";
   }
+}
+
+function display_totals() {
+  let froms = document.getElementsByClassName("from");
+  let from_values = new Array();
+  let from_countries = new Array();
+  let total = 0;
+
+  for (var i = 0; i < froms.length; i++) {
+    var item = froms[i];
+    total = total + parseInt(item.innerHTML);
+  }
+
+  var limit = (froms.length < 5) ? froms.length : 5;
+  for (var i = 0; i < limit; i++) {
+    var item = froms[i];
+    from_countries[i] = item.getAttribute('name');
+    from_values[i] = parseInt(item.innerHTML)*1.0 / total * 100;
+  }
+
+  if(froms.length > 5) {
+    from_countries[5] = "Others";
+    var others_value = 0;
+    for (var i = 5; i < froms.length; i++) {
+      var item = froms[i];
+      others_value = others_value + parseInt(item.innerHTML);
+    }
+    from_values[5] = others_value*1.0 / total * 100;
+  }
+
+  var colours_array = ["90e0ef","48cae4","00b4d8","0096c7","0077b6","023e8a","03045e"];
+
+  var data = [{
+    values: from_values,
+    labels: from_countries,
+    type: 'pie',
+    hoverinfo: 'label+percent',
+    marker: {
+      colors: colours_array
+    }
+  }];
+
+  var layout = {
+    paper_bgcolor: 'rgba(0,0,0,0)',
+    plot_bgcolor: 'rgba(0,0,0,0)',
+    font: {
+      color: 'white'
+    },
+    hoverlabel: {
+      bordercolor: 'white'
+    },
+    showlegend: false
+  }
+
+  Plotly.newPlot('countriespie', data, layout);
+}
+
+function display_ranking(country) {
+  let fors = document.getElementsByClassName("for");
+  let for_values = new Array();
+  let for_countries = new Array();
+  let total = 0;
+
+  for (var i = 0; i < fors.length; i++) {
+    var item = fors[i];
+    total = total + parseInt(item.innerHTML);
+  }
+
+  var limit = (fors.length < 5) ? fors.length : 5;
+  for (var i = 0; i < limit; i++) {
+    var item = fors[i];
+    for_countries[i] = item.getAttribute('name');
+    for_values[i] = parseInt(item.innerHTML)*1.0 / total * 100;
+  }
+
+  if(fors.length > 5) {
+    for_countries[5] = "Others";
+    var others_value = 0;
+    for (var i = 5; i < fors.length; i++) {
+      var item = fors[i];
+      others_value = others_value + parseInt(item.innerHTML);
+    }
+    for_values[5] = others_value*1.0 / total * 100;
+  }
+
+  var colours_array = ["90e0ef","48cae4","00b4d8","0096c7","0077b6","023e8a","03045e"];
+
+  var data = [{
+    values: for_values,
+    labels: for_countries,
+    type: 'pie',
+    hoverinfo: 'label+percent',
+    marker: {
+      colors: colours_array
+    }
+  }];
+
+  var layout = {
+    paper_bgcolor: 'rgba(0,0,0,0)',
+    plot_bgcolor: 'rgba(0,0,0,0)',
+    font: {
+      color: 'white'
+    },
+    hoverlabel: {
+      bordercolor: 'white'
+    },
+    showlegend: false
+  }
+
+  el_name = country + "_pie";
+  Plotly.newPlot(el_name, data, layout);
 }
--- statistics.php
+++ statistics.php
@@ -42,6 +42,119 @@
     $votesperartist["$country"] = $row['tot_votes'];
   }
   ?>
+  <script type="text/javascript">
+  function display_totals() {
+    let froms = document.getElementsByClassName("from");
+    let from_values = new Array();
+    let from_countries = new Array();
+    let total = 0;
+
+    for (var i = 0; i < froms.length; i++) {
+      var item = froms[i];
+      total = total + parseInt(item.innerHTML);
+    }
+
+    var limit = (froms.length < 5) ? froms.length : 5;
+    for (var i = 0; i < limit; i++) {
+      var item = froms[i];
+      from_countries[i] = item.getAttribute('name');
+      from_values[i] = parseInt(item.innerHTML)*1.0 / total * 100;
+    }
+
+    if(froms.length > 5) {
+      from_countries[5] = "Others";
+      var others_value = 0;
+      for (var i = 5; i < froms.length; i++) {
+        var item = froms[i];
+        others_value = others_value + parseInt(item.innerHTML);
+      }
+      from_values[5] = others_value*1.0 / total * 100;
+    }
+
+    var colours_array = ["90e0ef","48cae4","00b4d8","0096c7","0077b6","023e8a","03045e"];
+
+    var data = [{
+      values: from_values,
+      labels: from_countries,
+      type: 'pie',
+      hoverinfo: 'label+percent',
+      marker: {
+        colors: colours_array
+      }
+    }];
+
+    var layout = {
+      paper_bgcolor: 'rgba(0,0,0,0)',
+      plot_bgcolor: 'rgba(0,0,0,0)',
+      font: {
+        color: 'white'
+      },
+      hoverlabel: {
+        bordercolor: 'white'
+      },
+      showlegend: false
+    }
+
+    Plotly.newPlot('countriespie', data, layout);
+  }
+
+  function display_ranking(country) {
+    let fors = document.getElementsByClassName("for");
+    let for_values = new Array();
+    let for_countries = new Array();
+    let total = 0;
+
+    for (var i = 0; i < fors.length; i++) {
+      var item = fors[i];
+      total = total + parseInt(item.innerHTML);
+    }
+
+    var limit = (fors.length < 5) ? fors.length : 5;
+    for (var i = 0; i < limit; i++) {
+      var item = fors[i];
+      for_countries[i] = item.getAttribute('name');
+      for_values[i] = parseInt(item.innerHTML)*1.0 / total * 100;
+    }
+
+    if(fors.length > 5) {
+      for_countries[5] = "Others";
+      var others_value = 0;
+      for (var i = 5; i < fors.length; i++) {
+        var item = fors[i];
+        others_value = others_value + parseInt(item.innerHTML);
+      }
+      for_values[5] = others_value*1.0 / total * 100;
+    }
+
+    var colours_array = ["90e0ef","48cae4","00b4d8","0096c7","0077b6","023e8a","03045e"];
+
+    var data = [{
+      values: for_values,
+      labels: for_countries,
+      type: 'pie',
+      hoverinfo: 'label+percent',
+      marker: {
+        colors: colours_array
+      }
+    }];
+
+    var layout = {
+      paper_bgcolor: 'rgba(0,0,0,0)',
+      plot_bgcolor: 'rgba(0,0,0,0)',
+      font: {
+        color: 'white'
+      },
+      hoverlabel: {
+        bordercolor: 'white'
+      },
+      showlegend: false
+    }
+
+    el_name = country + "_pie";
+    Plotly.newPlot(el_name, data, layout);
+  }
+
+  </script>
 </head>
 <body>
   <nav id="menu">
@@ -55,81 +168,36 @@
     <h1>Votes statistics</h1>
     <h2>2022</h2>
   </header>
-  <section id="statistics-container">
-    <article class="values">
-      <span name="countries">
-        <?php
-        foreach(array_keys($votespercountry) as $key) {
-          echo "<div name='$key' class='from' hidden>$votespercountry[$key]</div>";
-        }
-        ?>
-      </span>
-      <span name="artists">
-        <?php
-        foreach(array_keys($votesperartist) as $key) {
-          echo "<div name='$key' class='for' hidden>$votesperartist[$key]</div>";
-        }
-        ?>
-      </span>
-    </article>
-    <article class="countries_displayer">
+  <section id="countries_container" class="countries_container">
       <div id="countriespie"></div>
-      <script type="text/javascript">
-      let froms = document.getElementsByClassName("from");
-      let from_values = new Array();
-      let from_countries = new Array();
-      let total = 0;
-
-      for (var i = 0; i < froms.length; i++) {
-        var item = froms[i];
-        total = total + parseInt(item.innerHTML);
-      }
-
-      var limit = (froms.length < 5) ? froms.length : 5;
-      for (var i = 0; i < limit; i++) {
-        var item = froms[i];
-        from_countries[i] = item.getAttribute('name');
-        from_values[i] = parseInt(item.innerHTML)*1.0 / total * 100;
-      }
-
-      if(froms.length > 5) {
-        from_countries[5] = "Others";
-        var others_value = 0;
-        for (var i = 5; i < froms.length; i++) {
-          var item = froms[i];
-          others_value = others_value + parseInt(item.innerHTML);
+      <details>
+      <summary>Number of user from each country</summary>
+      <table id="countries_votes">
+        <?php
+        $counter = 0;
+        echo "<tr><th>Country</th><th>Users</th></tr>";
+        foreach(array_keys($votespercountry) as $key) {
+          echo "<tr><td>$key</td> <td name='$key' class='from'>$votespercountry[$key]</td></tr>";
         }
-        from_values[5] = others_value*1.0 / total * 100;
-      }
-
-      var colours_array = ["90e0ef","48cae4","00b4d8","0096c7","0077b6","023e8a","03045e"];
-
-      var data = [{
-        values: from_values,
-        labels: from_countries,
-        type: 'pie',
-        hoverinfo: 'label+percent',
-        marker: {
-          colors: colours_array
-        }
-      }];
-
-      var layout = {
-        paper_bgcolor: 'rgba(0,0,0,0)',
-        plot_bgcolor: 'rgba(0,0,0,0)',
-        font: {
-          color: 'white'
-        },
-        hoverlabel: {
-          bordercolor: 'white'
-        },
-        showlegend: false
-      }
-
-      Plotly.newPlot('countriespie', data, layout);
-
+        ?>
+      </table>
+    </details>
+      <script>
+        display_totals()
       </script>
-    </article>
+  </section>
+  <section id="artists_container" class="artists_container">
+    <?php
+    $order = 1;
+    foreach(array_keys($votesperartist) as $key) {
+      echo "<details><summary>$order. $key</summary>";
+      echo "<div name='$key' class='for' hidden>$votesperartist[$key]</div>";
+      echo "<p id='$key"."_pie'></p>";
+      echo "</details>";
+      $order++;
+      echo "<script type='text/javascript'>display_ranking('".$key."');</script>";
+    }
+    ?>
   </section>
 </body>
 </html>
